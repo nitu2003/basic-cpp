65f4b487ecdfecdfb6db4fb104385660d6f73c68
Harry <nitukumari@.com>
2022-11-08 21:50:37 +0530

    intialcommit

---
+++ basic.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int sum(int x ,  int y){
+    cout<<x<<" # "<<y<<endl;
+    int data =x+y;
+    return data;
+}
+
+int multiply(int x, int y){
+    cout<<x<<" & "<<y<<endl;
+    int data =x*y;
+    return data;
+}
+
+int main(){
+    float a,b;
+    cout<<"addition of two number"<<endl;
+    cout<<"enter two number"<<endl;
+    cin>>a>>b;
+    int cal =multiply(a,b);
+    int result =sum(a , b );
+     cout<<cal<<endl;
+    cout<<result<<endl;
+}
+
---
+++ loop.cpp
@@ -0,0 +1,193 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// int main(){
+//     for(int i=9 ; i >=0 ; i--){
+//         cout<<i<<endl;
+//     }
+// }
+
+// int main(){
+//     for(int i=2;i<=10;i+=2){
+//      cout<<i<<endl;
+//     }
+// }
+
+// output
+//  *
+//  **
+//  ***
+//  ****
+// code
+//  int main(){
+//      for(int i=1;i<=4;i++){
+//          for(int j=0;j<i;j++){
+//              cout<<"*";
+//          }
+//          cout<<endl;
+//      }
+//  }
+
+// 1
+// 2 3
+// 4 5 6
+// int main()
+// {
+//     int a = 1;
+
+//     for (int i = 1; i <= 3; i++)
+//     {
+//         for (int j = 0; j < i; j++)
+//         {
+//             cout << a;
+//             a++;
+//         }
+//         cout << endl;
+//     }
+// }
+ 
+
+
+
+//1
+//12
+//123
+//1234
+// int main(){
+//     for (int i=1;i<=4;i++){
+//         for(int j=1;j <=i;j++){
+//             cout<<j;
+//         }
+//         cout<<endl;
+//     }
+// }
+
+//line 31 practice
+
+// int main(){
+//     int a=1;
+//     for(int i=1;i<=4;i++){
+//         for(int j=0;j<i;j++){
+//             cout<<a++;
+//         }
+//         cout<<endl;
+
+//     }
+
+// }
+
+//9876543210
+// int main(){
+//     for (int i=9;i>=0;i--){
+//         cout<<i<<endl;
+//     }
+// }
+
+
+//1
+//2 3
+//4 5 6
+//7 8 9 10
+//11 12 13 14 15
+
+// int main(){
+//     int a=1;
+//     for(int i=1;i<=6;i++){
+//         for(int j=1;j<i;j++){
+//             cout<<a<<" ";
+//             a++;
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// ****
+// ****
+// ****
+// ****
+
+// int main(){
+
+//     for(int i=1;i<=4;i++){
+//         for(int j=1;j<=4;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+// ****
+// *  *
+// *  *
+// ****
+// int main(){
+//     for(int i=1;i<=4;i++){
+//         for(int j=1;j<=4;j++){
+//             if(i==1 ||i==4){
+//                 cout<<"*";
+//             }
+//             else if (j==1||j==4){
+//                 cout<<"*";
+//             }
+//             else{
+//                 cout<<" ";
+//             }
+            
+//         }
+//     }
+// }
+
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
+// int main(){
+//     for(int i=5;i>=1;i--){
+//         for(int j=1;j<=i;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+//         * 
+//       * *
+//     * * *
+//   * * * *
+// * * * * *
+
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for ( int j=1;j<=5;j++){
+//             if(j<=5-i){
+//                 cout<<" ";
+//             }
+//             else{
+//                 cout<<"*";
+//             }
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// 1
+// 2 2
+// 3 3 3
+// 4 4 4 4
+// 5 5 5 5 5
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=i;j++){
+//             cout<<i<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+
+
+
+
